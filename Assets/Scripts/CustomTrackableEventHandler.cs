cb3b3baf44e13babf43871067af7682e169381ed
Luciano Nahuel Vitale <lunavi97@gmail.com>
2019-12-29 06:30:14 -0300

    Created custom trackable event handler and overridden functions

---
+++ Assets/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+    }
+}
